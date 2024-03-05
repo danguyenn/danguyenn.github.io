a3ab6268be0d0fffb441a1aa7941311ab795b7d0
buckethatdavid <daviddddnguyennnn@gmail.com>
2024-03-05 12:43:49 -0800

    implement menu active hover styling

--- david-website/src/pages/Home.js
+++ david-website/src/pages/Home.js
@@ -2,7 +2,12 @@ import './Home.css';
 const Home = () => {
     return(
         <div className='container'>
-            
+            <div className='intro'>
+                <p>intro</p>
+            </div>
+            <div className='content'>
+                <p>content</p>
+            </div>
         </div>
     );
 };
